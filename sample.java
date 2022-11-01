2e48f36d4794a684def47576826918c218bd2801
bharath <105805612+bharath34560sky@users.noreply.github.com>
2022-11-01 12:06:02 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class inputs
+ {
+	public static void main (String args[])
+	{
+		int number1,number2;
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter the two number:");
+		number1=in.nextInt();
+		number2=in.nextInt();
+		System.out.println("The number1 is ="+number1);
+		System.out.println("The number2 is ="+number2);
+	}
+}
